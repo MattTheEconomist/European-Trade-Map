f1714169584ebc2abaab4eef1e1b2094ba98e518
Matt Ashford <mattashford1@gmail.com>
2021-07-28 12:15:25 -0400

    obtained centroids data

--- src/graphic/capitals.js
+++ src/graphic/capitals.js
@@ -1,27 +1,36 @@
 import capitals from "../data/capitals.json";
 import countryList from "../data/countryList";
 
+import centroids from "../data/centroids.json";
+
 import { europeProjection } from "../mapDataPrep/mapDrawFunctions";
+import M from "minimatch";
+
+const removeTheseCountries = [
+  "Albania",
+  "Macedonia",
+  "Malta",
+  "Cyprus",
+  "Maldova",
+];
+
+const makeTheseSmaller = ["Portugal", "Switzerland", "Bosnia", "Denmark"];
 
 export function drawCapitals(svg) {
-  const euroCapitals = capitals.filter((row) =>
-    countryList.includes(row.country)
-  );
-  // console.log(euroCapitals);
-
   svg
     .selectAll("text")
-    .data(euroCapitals)
+    .data(centroids)
     .enter()
     .append("text")
     .attr("x", function (d) {
       return europeProjection([d.longitude, d.latitude])[0];
     })
     .attr("y", function (d) {
-      return europeProjection([d.longitude, d.latitude])[1] - 10;
+      return europeProjection([d.longitude, d.latitude])[1];
     })
-    .text((d) => d.capital)
-    .style("font-size", "10px");
+    .text((d) => d.name)
+    .style("font-size", "8px")
+    .attr("text-anchor", "middle");
 }
 
 export default drawCapitals;
